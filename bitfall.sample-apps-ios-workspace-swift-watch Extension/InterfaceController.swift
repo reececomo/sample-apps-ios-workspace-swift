3b5aa3ed6c7921da9c276d975986f910734d4b66
Akos Birmacher <akos.birmacher@bitrise.io>
2018-08-07 11:16:40 +0200

    watchkitapp added

---
+++ bitfall.sample-apps-ios-workspace-swift-watch Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  bitfall.sample-apps-ios-workspace-swift-watch Extension
+//
+//  Created by Akos Birmacher on 2018. 08. 07..
+//  Copyright © 2018. Bitfall. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
